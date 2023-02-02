de835dd5b7f8093269522574429dd71a8cbeceb8
Feyza Ürkut <feyzaurkut1@gmail.com>
2023-02-02 12:16:58 +0300

    Revert "Update main activity"
    This reverts commit ca0b4e3318037e392990fb1728b689135fb5b7ab.

--- app/src/main/java/com/myapps/jetpackcomposeproject/MainActivity.kt
+++ app/src/main/java/com/myapps/jetpackcomposeproject/MainActivity.kt
@@ -3,21 +3,122 @@ package com.myapps.jetpackcomposeproject
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.rememberNavController
-import com.myapps.jetpackcomposeproject.components.SetupNavGraph
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusOrder
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.myapps.jetpackcomposeproject.ui.theme.JetpackComposeProjectTheme
 
 class MainActivity : ComponentActivity() {
-
-    private lateinit var navController: NavHostController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             JetpackComposeProjectTheme {
-                navController = rememberNavController()
-                SetupNavGraph(navController = navController)
+                Login()
             }
         }
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun Login() {
+    val focusRequester = FocusRequester()
+    val focusManager = LocalFocusManager.current
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(24.dp),
+        verticalArrangement = Arrangement.spacedBy(18.dp, Alignment.Bottom),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "MyApp",
+            style = MaterialTheme.typography.h2,
+            modifier = Modifier.padding(bottom = 18.dp)
+        )
+        TextInput(InputType.Email, keyboardActions = KeyboardActions(onNext = {
+            focusRequester.requestFocus()
+        }))
+        TextInput(InputType.Password, keyboardActions = KeyboardActions(onDone = {
+            focusManager.clearFocus()
+        }), focusRequester = focusRequester)
+        Button(
+            onClick = { /*TODO*/ },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 58.dp),
+            shape = RoundedCornerShape(27.dp)
+        ) {
+            Text(text = "Login", modifier = Modifier.padding(vertical = 8.dp))
+        }
+    }
+}
+
+@Composable
+fun TextInput(
+    inputType: InputType,
+    focusRequester: FocusRequester? = null,
+    keyboardActions: KeyboardActions
+) {
+    var value by remember { mutableStateOf("") }
+    OutlinedTextField(
+        value = value,
+        onValueChange = { value = it },
+        modifier = Modifier
+            .fillMaxWidth()
+            .focusOrder(focusRequester ?: FocusRequester()),
+        shape = RoundedCornerShape(27.dp),
+        singleLine = true,
+        label = { Text(text = inputType.label) },
+        keyboardActions = keyboardActions,
+        keyboardOptions = inputType.keyboardOptions,
+        visualTransformation = inputType.visualTransformation,
+        leadingIcon = { Icon(imageVector = inputType.icon, contentDescription = null) }
+    )
+}
+
+sealed class InputType(
+    val label: String,
+    val icon: ImageVector,
+    val keyboardOptions: KeyboardOptions,
+    val visualTransformation: VisualTransformation
+) {
+    object Email : InputType(
+        label = "Email",
+        icon = Icons.Default.Person,
+        keyboardOptions = KeyboardOptions(
+            imeAction = ImeAction.Next,
+            keyboardType = KeyboardType.Email
+        ),
+        visualTransformation = VisualTransformation.None
+    )
+
+    object Password : InputType(
+        label = "Password",
+        icon = Icons.Default.Lock,
+        keyboardOptions = KeyboardOptions(
+            imeAction = ImeAction.Done,
+            keyboardType = KeyboardType.Password
+        ),
+        visualTransformation = PasswordVisualTransformation()
+    )
 }
