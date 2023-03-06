b51de590d4c18e0e682eb94a87125fb3e39413fc
Feyza Ürkut <feyzaurkut1@gmail.com>
2023-03-06 23:06:42 +0300

    Add new colors

--- app/src/main/java/com/myapps/jetpackcomposeproject/ui/theme/Color.kt
+++ app/src/main/java/com/myapps/jetpackcomposeproject/ui/theme/Color.kt
@@ -6,3 +6,7 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+val Blue = Color(0xFFc6e0f5)
+val Purple = Color(0xFFe0ccfc)
+val Orange = Color(0xFFf0d9ca)
