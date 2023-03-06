aff49f1a20c78da3af9f7ecf4db9de6d531bc809
Feyza Ürkut <feyzaurkut1@gmail.com>
2023-03-06 23:09:42 +0300

    Add onboarding

--- app/src/main/java/com/myapps/jetpackcomposeproject/components/Navigation.kt
+++ app/src/main/java/com/myapps/jetpackcomposeproject/components/Navigation.kt
@@ -4,14 +4,20 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import com.google.accompanist.pager.ExperimentalPagerApi
 import com.myapps.jetpackcomposeproject.data.model.Screen
 import com.myapps.jetpackcomposeproject.ui.home.Home
 import com.myapps.jetpackcomposeproject.ui.login.Login
+import com.myapps.jetpackcomposeproject.ui.onboarding.OnBoarding
 import com.myapps.jetpackcomposeproject.ui.signup.Signup
 
+@ExperimentalPagerApi
 @Composable
 fun SetupNavGraph(navController: NavHostController){
-    NavHost(navController = navController, startDestination = Screen.Login.route) {
+    NavHost(navController = navController, startDestination = Screen.OnBoarding.route) {
+        composable(Screen.OnBoarding.route) {
+            OnBoarding(navController)
+        }
         composable(Screen.Login.route) {
             Login(navController)
         }
--- app/src/main/java/com/myapps/jetpackcomposeproject/data/model/Screen.kt
+++ app/src/main/java/com/myapps/jetpackcomposeproject/data/model/Screen.kt
@@ -1,6 +1,7 @@
 package com.myapps.jetpackcomposeproject.data.model
 
 sealed class Screen(val route: String){
+    object OnBoarding: Screen(route = "onboarding_screen")
     object Login: Screen(route = "login_screen")
     object Signup: Screen(route = "signup_screen")
     object Home: Screen(route = "home_screen")
